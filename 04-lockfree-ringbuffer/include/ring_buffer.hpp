b25bbc8d0aa8384e15b0603e88ba4cbe6aef16ea
yuyatng27828 <yuyatng27828@gmail.com>
2025-06-07 19:47:59 +0900

    Define RingBuffer class

---
+++ 04-lockfree-ringbuffer/include/ring_buffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <cstddef>
+
+template <class T>
+class RingBuffer {
+    public:
+        explicit RingBuffer(size_t size);
+        
+        void put(T item);          // Add an item to the buffer
+        T get();                   // Get and remove the oldest item
+        void reset();              // Reset the buffer state
+        
+        bool empty() const;        // Check if the buffer is empty
+        bool full() const;         // Check if the buffer is full
+        size_t capacity() const;   // Max number of items
+        size_t size() const;       // Current number of items
+
+    private:
+        std::mutex mutex_;         // Thread safety 
+        std::unique_ptr<T[]> buf_; // Buffer storage
+        size_t head = 0;           // Write index
+        size_t tail = 0;           // Read index
+        const size_t max_size;     // Fixed buffer capacity
+        bool full_ = false;        // Full flag
+
+        void advance_head();       // Move write index 
+        void advance_tail();       // Move read index
+};
+
+#include "../src/RingBuffer.tpp"
